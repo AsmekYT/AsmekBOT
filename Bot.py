7c576dfc35e553022993f12272a2fc5041c2e752
AsmekYT <98432786+AsmekYT@users.noreply.github.com>
2023-04-14 10:16:56 +0200

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -1,5 +1,5 @@
 #główne komendy inportujące nakładkę discorda do pliku wykonawczego pythona
-import discord
+import discord.py
 from discord.ext import commands
 from discord import app_commands 
 from discord.ext.commands import has_permissions
