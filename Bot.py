fe650ea5655f19f2db22142af264aced5990a65d
AsmekYT <98432786+AsmekYT@users.noreply.github.com>
2023-04-19 19:49:00 +0200

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -175,7 +175,7 @@ async def ball(ctx, wiadomość: str):
     
     # Sprawdź, czy wiadomość to znak lub znaki interpunkcyjne
     if all(c in string.punctuation for c in wiadomość):
-        odpowiedz = "Tak"
+        ctx.respond('pytanie musi zawierać litery') 
     else:
         # Usuń wszystkie znaki interpunkcyjne i zamień na małe litery
         wiadomość = ''.join(c for c in wiadomość if c not in string.punctuation).lower()
