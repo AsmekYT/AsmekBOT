b54a79434dcea3d095a9bdafe28a209e5239dab5
AsmekYT <98432786+AsmekYT@users.noreply.github.com>
2023-04-23 19:41:37 +0200

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -167,7 +167,7 @@ async def unban(ctx, użytkownik: discord.User):
     await ctx.send(f"{user.mention} został odblokowany.")
 
 @client.slash_command()
-async def banlist(ctx: SlashContext):
+async def banlist(ctx):
     ban_list = await ctx.guild.bans()
     banned_users = [f"{user.name}#{user.discriminator} ({user.id})" for ban_entry in ban_list if (user := ban_entry.user)]
     if not banned_users:
